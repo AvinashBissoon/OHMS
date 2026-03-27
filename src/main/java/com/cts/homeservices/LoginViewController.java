8ff50a6bc0a4559db70145a82ad476bf966602d1
avinash <avinash2005b@gmail.com>
2026-03-26 22:36:29 -0600

    main

--- src/main/java/com/cts/homeservices/LoginViewController.java
+++ src/main/java/com/cts/homeservices/LoginViewController.java
@@ -39,11 +39,42 @@ public class LoginViewController implements Initializable {
     private Hyperlink btnRegisterLink;
 
 
-    //Populate ComboBox
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //Populate ComboBox
         cBoxLoginAccType.setItems(FXCollections.observableArrayList("Customer", "Worker", "Admin"));
-        cBoxLoginAccType.setStyle("-fx-font: 15px\"Ariel\";");
+
+        //Setting up the styles to be used
+        String defaultStyle = "-fx-background-radius: 15; -fx-background-color: white; -fx-border-radius: 15; -fx-border-color: #cccccc; -fx-border-width:1; ";
+        String focusedStyle = "-fx-background-radius: 15; -fx-background-color: white; -fx-border-radius: 15; -fx-border-color:  #036248; -fx-border-width:2; ";
+        String btnNormal = "-fx-background-color:  white; -fx-text-fill: #7c5050; -fx-background-radius: 15; -fx-font-weight:bold;";
+        String btnHover = "-fx-background-color: #036248; -fx-text-fill: white; -fx-background-radius: 15; -fx-font-weight:bold;";
+
+        //Text Field style on event
+        tbLoginEmail.setStyle(defaultStyle);
+        tbLoginEmail.focusedProperty().addListener((observableValue, oldVal, newVal) ->{
+            if (newVal) tbLoginEmail.setStyle(focusedStyle);
+            else tbLoginEmail.setStyle(defaultStyle);
+        });
+
+        tbLoginPassword.setStyle(defaultStyle);
+        tbLoginPassword.focusedProperty().addListener((observableValue, oldVal, newVal) -> {
+            if (newVal) tbLoginPassword.setStyle(focusedStyle);
+            else tbLoginPassword.setStyle(defaultStyle);
+        });
+        //Combo Box style on event
+        String comboFont = "-fx-font: 15px\"Ariel\";";
+        cBoxLoginAccType.setStyle(defaultStyle + comboFont);
+        cBoxLoginAccType.focusedProperty().addListener((observableValue, oldVal, newVal) -> {
+            if (newVal) cBoxLoginAccType.setStyle(focusedStyle + comboFont);
+            else cBoxLoginAccType.setStyle(defaultStyle + comboFont);
+        });
+
+        btnLogin.setStyle(btnNormal);
+        btnLogin.setOnMouseEntered(mouseEvent -> btnLogin.setStyle(btnHover));
+        btnLogin.setOnMouseExited(mouseEvent -> btnLogin.setStyle(btnNormal));
+
     }
 
 
