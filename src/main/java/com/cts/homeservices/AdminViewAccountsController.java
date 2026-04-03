ecd88320da1eed92257e45ee22162097b9a987d2
avinash <avinash2005b@gmail.com>
2026-04-03 00:06:33 -0600

    main

--- src/main/java/com/cts/homeservices/AdminViewAccountsController.java
+++ src/main/java/com/cts/homeservices/AdminViewAccountsController.java
@@ -1,11 +1,15 @@
 package com.cts.homeservices;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,7 +22,90 @@ public class AdminViewAccountsController implements Initializable {
     }
 
     @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private Tab tabCustomer;
+
+    @FXML
+    private Tab tabEmployee;
+
+    @FXML
+    private TableView<Customer> tblViewCustomer;
+
+    @FXML
+    private TableColumn<Customer, String> colCustFirstName;
+    @FXML
+    private TableColumn<Customer, String> colCustLastName;
+    @FXML
+    private TableColumn<Customer, String> colCustEmail;
+    @FXML
+    private TableColumn<Customer, String> colCustMobileNum;
+    @FXML
+    private TableColumn<Customer, String> colCustAddress1;
+    @FXML
+    private TableColumn<Customer, String> colCustAddress2;
+    @FXML
+    private TableColumn<Customer, String> colCustCity;
+    @FXML
+    private TableColumn<Customer, String> colCustCountry;
+
+    @FXML
+    private TableView<Employee> tblViewEmployee;
+    @FXML
+    private TableColumn<Customer, String> colEmpFirstName;
+    @FXML
+    private TableColumn<Customer, String> colEmpLastName;
+    @FXML
+    private TableColumn<Customer, String> colEmpEmail;
+    @FXML
+    private TableColumn<Customer, String> colEmpMobileNum;
+    @FXML
+    private TableColumn<Customer, String> colEmpAddress1;
+    @FXML
+    private TableColumn<Customer, String> colEmpAddress2;
+    @FXML
+    private TableColumn<Customer, String> colEmpCity;
+    @FXML
+    private TableColumn<Customer, String> colEmpCountry;
+
+    @FXML
+    private TextField tbFirstName;
+
+    @FXML
+    private TextField tbLastName;
+
+    @FXML
+    private TextField tbEmail;
+
+    @FXML
+    private TextField tbMobileNumber;
+
+    @FXML
+    private TextField tbAddressLine1;
+
+    @FXML
+    private TextField tbAddressLine2;
+
+    @FXML
+    private TextField tbCity;
+
+    @FXML
+    private TextField tbCountry;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
     private Button btnReturnToHome;
+
+    @FXML
+    private ObservableList<Customer> customerList = FXCollections.observableArrayList();
+
+    @FXML
+    private ObservableList<Employee> employeeList = FXCollections.observableArrayList();
+
+
 
 
     @Override
@@ -31,7 +118,117 @@ public class AdminViewAccountsController implements Initializable {
         btnReturnToHome.setStyle(btnNormal);
         btnReturnToHome.setOnMouseEntered(mouseEvent -> btnReturnToHome.setStyle(btnHover));
         btnReturnToHome.setOnMouseExited(mouseEvent -> btnReturnToHome.setStyle(btnNormal));
+
+
+
+        //Map customer columns
+        colCustFirstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        colCustLastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        colCustEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colCustMobileNum.setCellValueFactory(new PropertyValueFactory<>("mobilePhone"));
+        colCustAddress1.setCellValueFactory(new PropertyValueFactory<>("streetAddress1"));
+        colCustAddress2.setCellValueFactory(new PropertyValueFactory<>("streetAddress2"));
+        colCustCity.setCellValueFactory(new PropertyValueFactory<>("city"));
+        colCustCountry.setCellValueFactory(new PropertyValueFactory<>("country"));
+
+
+        //map employee columns
+        colEmpFirstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        colEmpLastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        colEmpEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colEmpMobileNum.setCellValueFactory(new PropertyValueFactory<>("mobilePhone"));
+        colEmpAddress1.setCellValueFactory(new PropertyValueFactory<>("streetAddress1"));
+        colEmpAddress2.setCellValueFactory(new PropertyValueFactory<>("streetAddress2"));
+        colEmpCity.setCellValueFactory(new PropertyValueFactory<>("city"));
+        colEmpCountry.setCellValueFactory(new PropertyValueFactory<>("country"));
+
+        tblViewCustomer.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) ->{
+            if (newSelection != null) {
+                tbFirstName.setText(String.valueOf(newSelection.getFirstName()));
+                tbLastName.setText(newSelection.getLastName());
+                tbEmail.setText(newSelection.getEmail());
+                tbMobileNumber.setText(String.valueOf(newSelection.getMobilePhone()));
+                tbAddressLine1.setText(newSelection.getStreetAddress1());
+                tbAddressLine2.setText(newSelection.getStreetAddress2());
+                tbCity.setText(newSelection.getCity());
+                tbCountry.setText(newSelection.getCountry());
+            }
+        });
+
+        tblViewEmployee.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) ->{
+            if (newSelection != null) {
+                tbFirstName.setText(String.valueOf(newSelection.getFirstName()));
+                tbLastName.setText(newSelection.getLastName());
+                tbEmail.setText(newSelection.getEmail());
+                tbMobileNumber.setText(String.valueOf(newSelection.getMobilePhone()));
+                tbAddressLine1.setText(newSelection.getStreetAddress1());
+                tbAddressLine2.setText(newSelection.getStreetAddress2());
+                tbCity.setText(newSelection.getCity());
+                tbCountry.setText(newSelection.getCountry());
+            }
+        });
+
+        loadCustomerData();
+        loadEmployeeData();
     }
+
+    public void loadCustomerData(){
+        customerList.clear();
+        DatabaseConnection dc = new DatabaseConnection();
+
+        String query = "SELECT * FROM tblcustomer";
+
+        try {
+            dc.ps = dc.con.prepareStatement(query);
+            dc.rst = dc.ps.executeQuery();
+
+            while (dc.rst.next()) {
+                customerList.add(new Customer(
+                        dc.rst.getString("firstname"),
+                        dc.rst.getString("lastname"),
+                        dc.rst.getString("email"),
+                        dc.rst.getString("mobilephone"),
+                        dc.rst.getString("streetaddress1"),
+                        dc.rst.getString("streetaddress2"),
+                        dc.rst.getString("city"),
+                        dc.rst.getString("country")
+                ));
+            }
+            tblViewCustomer.setItems(customerList);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void loadEmployeeData(){
+       employeeList.clear();
+        DatabaseConnection dc = new DatabaseConnection();
+
+        String query = "SELECT * FROM tblemployee";
+
+        try {
+            dc.ps = dc.con.prepareStatement(query);
+            dc.rst = dc.ps.executeQuery();
+
+            while (dc.rst.next()) {
+                employeeList.add(new Employee(
+                        dc.rst.getString("firstname"),
+                        dc.rst.getString("lastname"),
+                        dc.rst.getString("email"),
+                        dc.rst.getString("mobilephone"),
+                        dc.rst.getString("streetaddress1"),
+                        dc.rst.getString("streetaddress2"),
+                        dc.rst.getString("city"),
+                        dc.rst.getString("country")
+                ));
+            }
+            tblViewEmployee.setItems(employeeList);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 
     @FXML
     private void returnToHome(ActionEvent event) throws IOException {
---
+++ src/main/java/com/cts/homeservices/Employee.java
@@ -0,0 +1,35 @@
+package com.cts.homeservices;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String streetAddress1;
+    private String streetAddress2;
+    private String city;
+    private String country;
+    private String mobilePhone;
+    private String email;
+
+    public Employee(String firstName, String lastName, String email, String mobilePhone, String streetAddress1,
+                    String streetAddress2, String city, String country) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.mobilePhone = mobilePhone;
+        this.streetAddress1 = streetAddress1;
+        this.streetAddress2 = streetAddress2;
+        this.city = city;
+        this.country = country;
+
+    }
+
+    // Getters for TableView
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
+    public String getMobilePhone() { return mobilePhone; }
+    public String getStreetAddress1() { return streetAddress1; }
+    public String getStreetAddress2() { return streetAddress2; }
+    public String getCity() { return city; }
+    public String getCountry() { return country; }
+}
